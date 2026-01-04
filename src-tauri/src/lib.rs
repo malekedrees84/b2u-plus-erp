7008bd620fadbae515da1850d080f89b7604402a
malek <malekedrees@maleks-MacBook-Pro.local>
2026-01-04 05:08:50 +0200

    release v0.1.0

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,16 +1,13 @@
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
+#[tauri::command]
+fn ping() -> String {
+  "pong".to_string()
+}
+
 pub fn run() {
   tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
+    .plugin(tauri_plugin_dialog::init())
+    .plugin(tauri_plugin_updater::Builder::new().build())
+    .invoke_handler(tauri::generate_handler![ping])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
